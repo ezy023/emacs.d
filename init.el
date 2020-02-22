9aeda86f947406a6978601becb01a1008cbb9330
Erik <allareri@gmail.com>
2020-02-22 22:09:37 +0000

    Add go-mode-hook to set tab width

--- init.el
+++ init.el
@@ -165,8 +165,8 @@
 ;; Go Lang
 (add-hook 'go-mode-hook (lambda ()
                           (progn
-                            (add-hook 'go-mode-hook (setq tab-width 4))
                             (add-hook 'before-save-hook 'gofmt-before-save))))
+(add-hook 'go-mode-hook #'(lambda () (setq tab-width 4)))
 
 
 
